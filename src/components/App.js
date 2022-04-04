dbfdddf9551ea255347b00df701fcb0ec7fe71bc
dalsoya <lookat77@naver.com>
2022-04-04 10:50:13 +0900

    chap 1 done

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppRouter from "./Router";
+
+function App() {
+  return (
+    <div id="root">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import firebase from './firebase'
+console.log(firebase)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
